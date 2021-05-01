85122c950e6661c2d6ccf52df91f252fb2440834
saurav012 <53165012+saurav012@users.noreply.github.com>
2021-05-01 16:17:41 +0530

    First commit

---
+++ src/app/Header-section/header-section.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  templateUrl:   './header-section.html' , 
+  styles: ['./header-section.css']
+})
+export class Headersection implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
